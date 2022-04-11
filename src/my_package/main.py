a56e256e4df2c6cf6209bf3f0bb2c00c21e1dd71
Daniel West <daniel.west.279@gmail.com>
2022-04-11 12:05:21 +1000

    Initial commit

---
+++ src/my_package/main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from my_package.print_hi import print_hi
+
+
+def main():
+    print_hi('PyCharm')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
